92c4044bb5d1a1405c62bb3b953028fe14d0275e
lm/223994 <leander.maigler@hs-weingarten.de>
2024-11-29 10:31:37 +0100

    modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
-const app = exoress();
+const app = express();
 const PORT = 2000;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend/index.html'));
 });
 
 app.listen(PORT, () => {
